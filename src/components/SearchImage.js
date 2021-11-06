70e00501d372ebc3cf92048f32e659a70eff7315
andurebel <andu.trica@gmail.com>
2021-11-06 23:00:43 +0200

    fixed input

--- src/components/SearchImage.js
+++ src/components/SearchImage.js
@@ -10,15 +10,15 @@ const SearchImage = ({ searchText }) => {
   return (
     <div className="max-w-sm rounded my-10 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
-        <div className="flex items-center border border-b-2 border-purple-500 py-2">
+        <div className="flex items-center  border-b border-purple-500 py-2   ">
           <input
             type="text"
             onChange={(event) => setText(event.target.value)}
-            className="appearence-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none"
-            placeholder="Search an image"
+            className="  focus:outline-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 "
+            placeholder="Search for an image"
           />
           <button
-            className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0  hover:bg-purple-700 hover:text-white border-purple-500 hover:border-purple-700 text-sm border-2 text-gray-700 py-1 px-2 mr-1 rounded"
             type="submit"
           >
             SEARCH
